fa5317ff528dcb54839514861d17d10e48fe78f7
guillermounivalle <guillermo.hernadez@correounivalle.edu.co>
2023-11-03 00:12:53 -0500

    Activity number 2 source code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //Import Libraries
-
+import logo from './logo.svg';
 import React, {Component} from 'react';
 import './App.css';
 
@@ -9,14 +9,65 @@ class App extends Component{
 
         this.state ={
             tasks: [],
-            newTask:{}
+            newTask:'' 
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.addTask = this.addTask.bind(this);
+        this.removeTask = this.removeTask.bind(this);
     }
+
+    addTask = () => {
+        const { tasks, newTask } = this.state;
+        if (newTask) {
+          this.setState({
+            tasks: [...tasks, newTask],
+            newTask: ''
+          });
+        }
+    };
+    
+    removeTask = (index) => {
+        const { tasks } = this.state;
+        tasks.splice(index, 1);
+        this.setState({ tasks: tasks });
+    };
+    
+    handleChange = (e) => {
+        this.setState({ newTask: e.target.value });
+    };
+    
+      
 
 
     render(){
+        //let taskslist = Object.keys(this.state.tasks);
+        const tasks = this.state.tasks;
         return(
-            <div>Primer paso</div>
+            <div className="App">
+                <h1>ACTIVIDAD 2  LISTA DE TAREAS</h1>
+                <div className="task-input">
+                    <input
+                        type="text"
+                        value={this.state.newTask}
+                        onChange={this.handleChange}
+                        placeholder="Add a new task"
+                        maxLength="255"/>
+                    <button onClick={this.addTask}>Add</button>
+                </div>
+                <ul>
+                    {tasks.map((task, index) => (
+                        <li key={index}>
+                        {task}
+                        <button 
+                        className='buttonList'
+                            onClick={() => this.removeTask(index)}>
+                                <i class="bi bi-trash-fill"></i>
+                        </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         );
     }
 }
